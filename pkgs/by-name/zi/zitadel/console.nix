079f3e08d155667cc8c91d05ca3a105ad178ae45
Xoddiel d'Croy <xoddiel.dcroy@gmail.com>
2025-12-14 23:09:16 +0100

    WIP: zitadel: update to v4.7.5

--- pkgs/by-name/zi/zitadel/console.nix
+++ pkgs/by-name/zi/zitadel/console.nix
@@ -5,23 +5,73 @@
 }:
 
 {
+  lib,
   stdenv,
-  fetchYarnDeps,
-  yarnConfigHook,
-  yarnBuildHook,
+  pnpm,
   nodejs,
-
   grpc-gateway,
+  protoc-gen-connect-go,
+  protoc-gen-es,
   protoc-gen-grpc-web,
-  protoc-gen-js,
+  buf,
+  fetchFromGitHub,
+  pkg-config,
+  protobuf_27,
 }:
 
 let
+  protoc-gen-js = stdenv.mkDerivation (finalAttrs: {
+    pname = "protoc-gen-js";
+    version = "3.21.4";
+
+    src = fetchFromGitHub {
+      owner = "protocolbuffers";
+      repo = "protobuf-javascript";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-eIOtVRnHv2oz4xuVc4aL6JmhpvlODQjXHt1eJHsjnLg=";
+    };
+
+    nativeBuildInputs = [
+      pkg-config
+      stdenv.cc
+    ];
+
+    buildInputs = [
+      protobuf_27
+      protobuf_27.passthru.abseil-cpp
+    ];
+
+    doCheck = false;
+
+    buildPhase = ''
+      runHook preBuild
+
+      $CXX -std=c++17 \
+        -I. \
+        $(pkg-config --cflags protobuf) \
+        generator/*.cc \
+        -o protoc-gen-js \
+        $(pkg-config --libs protobuf) \
+        -lprotoc
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      install -Dm755 protoc-gen-js $out/bin/protoc-gen-js
+
+      runHook postInstall
+    '';
+  });
+
   protobufGenerated = generateProtobufCode {
     pname = "zitadel-console";
     inherit version;
     nativeBuildInputs = [
       grpc-gateway
+      protoc-gen-connect-go
       protoc-gen-grpc-web
       protoc-gen-js
     ];
@@ -29,33 +79,100 @@ let
     bufArgs = "../proto --include-imports --include-wkt";
     outputPath = "src/app/proto";
   };
+
+  zitadelProtobufGenerated = generateProtobufCode {
+    pname = "zitadel-proto";
+    inherit version;
+    nativeBuildInputs = [
+      protoc-gen-es
+    ];
+    workDir = "packages/zitadel-proto";
+    bufArgs = "../../proto";
+    outputPath = ".";
+  };
+
+  client = stdenv.mkDerivation (finalAttrs: {
+    pname = "zitadel-client";
+    inherit version;
+    src = zitadelRepo;
+
+    pnpmDeps = pnpm.fetchDeps {
+      inherit (finalAttrs) pname version src;
+      fetcherVersion = 2;
+      hash = "sha256-P7CpmdyxJVv69mi4pYWkmrvT/mYDjYQa+RC7tJmCHv0=";
+    };
+
+    pnpmWorkspaces = [
+      "@zitadel/proto"
+      "@zitadel/client"
+    ];
+
+    nativeBuildInputs = [
+      pnpm.configHook
+      nodejs
+      buf
+    ];
+
+    preBuild = ''
+      cp -r ${zitadelProtobufGenerated}/{cjs,es,types} packages/zitadel-proto
+    '';
+
+    buildPhase = ''
+      runHook preBuild
+      pnpm --filter=@zitadel/client run build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      cp -r packages/zitadel-client/dist "$out"
+      runHook postInstall
+    '';
+  });
 in
-stdenv.mkDerivation {
+stdenv.mkDerivation (finalAttrs: {
   pname = "zitadel-console";
   inherit version;
 
   src = zitadelRepo;
 
-  sourceRoot = "${zitadelRepo.name}/console";
-
-  offlineCache = fetchYarnDeps {
-    yarnLock = "${zitadelRepo}/console/yarn.lock";
-    hash = "sha256-ekgLd5DTOBZWuT63QnTjx40ZYvLKZh+FXCn+h5vj9qQ=";
+  pnpmDeps = pnpm.fetchDeps {
+    inherit (finalAttrs) pname version src;
+    fetcherVersion = 2;
+    hash = "sha256-P7CpmdyxJVv69mi4pYWkmrvT/mYDjYQa+RC7tJmCHv0=";
   };
 
+  pnpmWorkspaces = [
+    "@zitadel/proto"
+    "@zitadel/client"
+    "console"
+  ];
+
   nativeBuildInputs = [
-    yarnConfigHook
-    yarnBuildHook
+    pnpm.configHook
     nodejs
+    buf
   ];
 
   preBuild = ''
-    cp -r ${protobufGenerated} src/app/proto
+    cp -r ${protobufGenerated} console/src/app/proto
+    cp -r ${zitadelProtobufGenerated}/{cjs,es,types} packages/zitadel-proto
+    cp -r ${client} packages/zitadel-client/dist
+  '';
+
+  buildPhase = ''
+    runHook preBuild
+
+    pnpm --filter=console build
+
+    runHook postBuild
   '';
 
   installPhase = ''
     runHook preInstall
-    cp -r dist/console "$out"
+
+    cp -r console/dist/console "$out"
+
     runHook postInstall
   '';
-}
+})
