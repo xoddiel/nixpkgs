6537b665bf7991cf7d0bd1bcce7b860b26225322
Xoddiel d'Croy <xoddiel.dcroy@gmail.com>
2025-12-14 19:33:47 +0100

    WIP: zitadel: manually fetch protobuf dependencies

--- pkgs/by-name/zi/zitadel/package.nix
+++ pkgs/by-name/zi/zitadel/package.nix
@@ -3,6 +3,7 @@
   buildGoModule,
   callPackage,
   fetchFromGitHub,
+  fetchurl,
   lib,
 
   buf,
@@ -13,6 +14,7 @@
   protoc-gen-validate,
   sass,
   statik,
+  yq-go,
 }:
 
 let
@@ -48,6 +50,56 @@ let
   protoc-gen-authoption = buildZitadelProtocGen "authoption";
   protoc-gen-zitadel = buildZitadelProtocGen "zitadel";
 
+  fetchProtobufDep =
+    {
+      remote ? "buf.build",
+      owner,
+      repo,
+      rev,
+      hash,
+      imports ? false,
+    }:
+    fetchurl {
+      pname = "${repo}-buf-dep";
+      version = rev;
+
+      url = "https://${remote}/${owner}/${repo}/archive/${rev}.tar.gz${if imports then "?imports=true" else ""}";
+
+      inherit hash;
+      recursiveHash = true;
+
+      downloadToTemp = true;
+
+      postFetch = ''
+        mkdir $out
+        tar -xvf "$downloadedFile" -C "$out"
+        echo 'version: v1' > "$out/buf.lock"
+      '';
+    };
+
+  protobufDeps = {
+    protoc-gen-validate = {
+      owner = "envoyproxy";
+      repo = "protoc-gen-validate";
+      rev = "6607b10f00ed4a3d98f906807131c44a";
+      hash = "sha256-8CN3p7wIRPTzLJoILnvjash6qFOdixUN7Wia5pdc/A8=";
+    };
+
+    googleapis = {
+      owner = "googleapis";
+      repo = "googleapis";
+      rev = "75b4300737fb4efca0831636be94e517";
+      hash = "sha256-TimZ6UdByjoKXb4BARydkzEn1Niy3IvXk4B8D/J1L1M=";
+    };
+
+    grpc-gateway = {
+      owner = "grpc-ecosystem";
+      repo = "grpc-gateway";
+      rev = "a1ecdc58eccd49aa8bea2a7a9022dc27";
+      hash = "sha256-J77NEgrawkNy3h8xVni/ah2IKdQVBf9bdxJ5DGPqWww=";
+    };
+  };
+
   # Buf downloads dependencies from an external repo - there doesn't seem to
   # really be any good way around it. We'll use a fixed-output derivation so it
   # can download what it needs, and output the relevant generated code for use
@@ -60,7 +112,6 @@ let
       bufArgs ? "",
       workDir ? ".",
       outputPath,
-      hash,
     }:
     stdenv.mkDerivation {
       pname = "${pname}-buf-generated";
@@ -72,20 +123,33 @@ let
       nativeBuildInputs = nativeBuildInputs ++ [
         buf
         cacert
+        yq-go
       ];
 
+      # We remove remote dependencies and manually attach our fetched dependencies
+      # to the project as directories. Sadly, buf demands relative paths, so we first
+      # have to create a symbolic link for each dependency.
       buildPhase = ''
+        yq --inplace '.deps = []' proto/buf.yaml
+        yq --inplace '.deps = []' proto/buf.lock
+        mkdir buf-vendor
+
+        ${lib.concatLines (
+          lib.flip lib.mapAttrsToList protobufDeps (
+            name: dep: ''
+              ln -s "${fetchProtobufDep dep}" "buf-vendor/${name}"
+              yq --inplace '.directories += ["buf-vendor/${name}"]' buf.work.yaml
+            ''
+          )
+        )}
+
         cd ${workDir}
-        HOME=$TMPDIR buf generate ${bufArgs}
+        HOME=$TMPDIR buf generate --debug ${bufArgs}
       '';
 
       installPhase = ''
         cp -r ${outputPath} $out
       '';
-
-      outputHashMode = "recursive";
-      outputHashAlgo = "sha256";
-      outputHash = hash;
     };
 
   protobufGenerated = generateProtobufCode {
@@ -100,7 +164,6 @@ let
       protoc-gen-zitadel
     ];
     outputPath = ".artifacts";
-    hash = "sha256-rc5A2bQ2iWkybprQ7IWsQ/LLAQxPqhlxzVvPn8Ec56E=";
   };
 in
 buildGoModule rec {
